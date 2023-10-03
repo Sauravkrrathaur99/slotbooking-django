4ed884f83caaac858fb6c21860d06c98af7fc072
Saurav Kumar Rathaur <saurav.20b0103004@abes.ac.in>
2023-10-03 18:06:01 +0530

    managed the user slots

--- slotbooking_app/views.py
+++ slotbooking_app/views.py
@@ -1,66 +1,3 @@
-# from django.shortcuts import render
-# from .forms import SlotBookingForm
-
-# # def slot_booking(request):
-# #     if request.method == 'POST':
-# #         form = SlotBookingForm(request.POST)
-# #         if form.is_valid():
-# #             # Add logic to check if the selected slot is available and save it if available
-# #             # For example, you can check if the slot is already booked by another candidate
-# #             # If it's available, you can mark it as booked and save it to the database
-# #             # You can also associate the slot with the candidate, if needed.
-# #             form.save()
-# #             # Redirect to a success page or a thank you page
-# #             return render(request, 'thank_you.html')
-# #     else:
-# #         form = SlotBookingForm()
-
-# #     return render(request, 'slotbooking.html', {'form': form})
-
-
-# from django.shortcuts import render, redirect
-# from .models import InterviewSlot
-# from .forms import SlotBookingForm
-
-# def slot_booking(request):
-#     available_slots = InterviewSlot.objects.filter(is_booked=False)
-#     if request.method == 'POST':
-#         form = SlotBookingForm(request.POST, available_slots=available_slots)
-#         if form.is_valid():
-#             selected_slot_id = form.cleaned_data['selected_slot']
-#             selected_slot = InterviewSlot.objects.get(pk=selected_slot_id)
-#             selected_slot.is_booked = True
-#             selected_slot.save()
-#             return redirect('thank_you')
-#     else:
-#         form = SlotBookingForm(available_slots=available_slots)
-#     return render(request, 'slotbooking.html', {'form': form, 'available_slots': available_slots})
-
-
-
-# from django.shortcuts import render, redirect
-# from .models import InterviewSlot
-# from .forms import SlotBookingForm
-
-# def slot_booking(request):
-#     # Filter available slots that are not booked
-#     available_slots = InterviewSlot.objects.filter(is_booked=False)
-    
-#     if request.method == 'POST':
-#         form = SlotBookingForm(request.POST)
-#         form.fields['selected_slot'].queryset = available_slots  # Set the queryset
-#         if form.is_valid():
-#             selected_slot_id = form.cleaned_data['selected_slot']
-#             selected_slot = InterviewSlot.objects.get(pk=selected_slot_id)
-#             selected_slot.is_booked = True
-#             selected_slot.save()
-#             return redirect('thank_you')
-#     else:
-#         form = SlotBookingForm()
-#         form.fields['selected_slot'].queryset = available_slots  # Set the queryset
-
-#     return render(request, 'slotbooking.html', {'form': form})
-
 
 # views.py
 import time
@@ -79,7 +16,7 @@ from django.core.mail import EmailMultiAlternatives
 def slot_booking(request):
     time_slots = InterviewSlot.objects.all()
     current_year = datetime.datetime.now().year
-    user = request.user
+    
     try:
         common_meeting_link = MeetingLink.objects.first().link
     except MeetingLink.DoesNotExist:
@@ -89,11 +26,12 @@ def slot_booking(request):
         if form.is_valid():
             email = form.cleaned_data['email']
             selected_slot = form.cleaned_data["slot"]
+            slot_user = selected_slot.user
             selected_slot.is_booked = True
             selected_slot.save()
             # Create and save a new instance of BookedSlots
             booked_slot = BookedSlots(
-                user=user,
+                user=slot_user,
                 email=email,
                 date=selected_slot.date,
                 time_start=selected_slot.time_start,
